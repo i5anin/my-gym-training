99da2f6e346ffb0ac7f5e22af27eda7079a42ad4
Isanin Sergey <sergey.isanin@gmail.com>
2025-02-06 23:00:14 +0300

    Update WorkoutForm.vue

--- client/src/modules/editor-finance/WorkoutForm.vue
+++ client/src/modules/editor-finance/WorkoutForm.vue
@@ -1,48 +1,100 @@
 <template>
   <v-form @submit.prevent="submitWorkout">
-    <v-row>
-      <v-col cols="12" md="4">
+    <v-row v-for="(exercise, exerciseIndex) in newWorkout.exercises" :key="'exercise-' + exerciseIndex">
+      <v-col cols="6" md="2">
         <v-text-field
-          v-model="newWorkout.workout_number"
+          v-model="exercise.workout_number"
           label="Workout Number"
           required
-        ></v-text-field>
+        />
       </v-col>
-      <v-col cols="12" md="4">
+      <v-col cols="6" md="2">
         <v-text-field
-          v-model="newWorkout.title"
+          v-model="exercise.title"
           label="Workout Title"
           required
-        ></v-text-field>
+        />
       </v-col>
-      <v-col cols="12" md="4">
+      <v-col cols="6" md="2">
         <v-select
-          v-model="newWorkout.muscle_group_id"
+          v-model="exercise.muscle_group_id"
           :items="muscleGroups"
           label="Muscle Group"
           item-text="name"
           item-value="id"
           required
-        ></v-select>
+        />
       </v-col>
-      <v-col cols="12" md="4">
+      <v-col cols="6" md="2">
         <v-select
-          v-model="newWorkout.exercise_type_id"
+          v-model="exercise.exercise_type_id"
           :items="exerciseTypes"
           label="Exercise Type"
           item-text="name"
           item-value="id"
           required
-        ></v-select>
+        />
       </v-col>
+      <v-col cols="6" md="2">
+        <v-btn @click="removeExercise(exerciseIndex)" color="error">Удалить упражнение</v-btn>
+      </v-col>
+
+      <!-- Сеты -->
+      <v-row v-for="(set, setIndex) in exercise.sets" :key="'set-' + exerciseIndex + '-' + setIndex">
+        <v-col cols="6" md="2">
+          <v-text-field
+            v-model="set.weight"
+            label="Вес (кг)"
+            type="number"
+            required
+          />
+        </v-col>
+        <v-col cols="6" md="2">
+          <v-text-field
+            v-model="set.repetitions"
+            label="Повторения"
+            type="number"
+            required
+          />
+        </v-col>
+        <v-col cols="6" md="2">
+          <v-btn @click="addDropSet(exerciseIndex, setIndex)" color="secondary">+ Добивка</v-btn>
+        </v-col>
+        <v-col cols="6" md="2">
+          <v-btn @click="removeSet(exerciseIndex, setIndex)" color="error">Удалить сет</v-btn>
+        </v-col>
+
+        <!-- Добивки -->
+        <v-row v-for="(dropSet, dropIndex) in set.extra" :key="'drop-' + exerciseIndex + '-' + setIndex + '-' + dropIndex">
+          <v-col cols="6" md="2">
+            <v-text-field
+              v-model="dropSet.weight"
+              label="Добивка: Вес (кг)"
+              type="number"
+            />
+          </v-col>
+          <v-col cols="6" md="2">
+            <v-text-field
+              v-model="dropSet.repetitions"
+              label="Добивка: Повторения"
+              type="number"
+            />
+          </v-col>
+          <v-col cols="6" md="2">
+            <v-btn @click="removeDropSet(exerciseIndex, setIndex, dropIndex)" color="error">Удалить добивку</v-btn>
+          </v-col>
+        </v-row>
+      </v-row>
+
+      <v-btn v-if="canAddSet(exercise)" @click="addSet(exerciseIndex)" color="secondary" class="mt-2">+ Сет</v-btn>
     </v-row>
 
-    <v-btn type="submit" color="primary">Add Workout</v-btn>
+    <v-btn @click="addExercise" color="secondary" class="mt-4">+ Добавить упражнение</v-btn>
+    <v-btn type="submit" color="primary" class="mt-4">Добавить тренировку</v-btn>
   </v-form>
 </template>
 
 <script>
-// Импортируем API-сервис
 import { workoutApi } from './api/workoutApi'
 
 export default {
@@ -53,36 +105,69 @@ export default {
   },
   data() {
     return {
-      newWorkout: {
-        workout_number: '',
-        title: '',
-        muscle_group_id: null,
-        exercise_type_id: null,
-        sets: [],
-      },
+      newWorkout: this.getDefaultWorkout(),
     }
   },
   methods: {
-    // Добавляем тренировку
     submitWorkout() {
       workoutApi
         .addWorkout(this.newWorkout)
         .then(() => {
-          this.$emit('workoutAdded') // Оповещаем родительский компонент о добавлении тренировки
-          this.newWorkout = {
-            workout_number: '',
-            title: '',
-            muscle_group_id: null,
-            exercise_type_id: null,
-            sets: [],
-          }
+          this.$emit('workoutAdded')
+          this.resetForm()
         })
-        .catch((error) => console.error('Error adding workout:', error))
+        .catch(console.error)
+    },
+    addExercise() {
+      this.newWorkout.exercises.push(this.getDefaultExercise())
+    },
+    removeExercise(index) {
+      this.newWorkout.exercises.splice(index, 1)
+    },
+    addSet(exerciseIndex) {
+      if (this.canAddSet(this.newWorkout.exercises[exerciseIndex])) {
+        this.newWorkout.exercises[exerciseIndex].sets.push(this.getDefaultSet())
+      }
+    },
+    removeSet(exerciseIndex, setIndex) {
+      this.newWorkout.exercises[exerciseIndex].sets.splice(setIndex, 1)
+    },
+    addDropSet(exerciseIndex, setIndex) {
+      this.newWorkout.exercises[exerciseIndex].sets[setIndex].extra.push({
+        weight: '',
+        repetitions: '',
+      })
+    },
+    removeDropSet(exerciseIndex, setIndex, dropIndex) {
+      this.newWorkout.exercises[exerciseIndex].sets[setIndex].extra.splice(dropIndex, 1)
+    },
+    resetForm() {
+      this.newWorkout = this.getDefaultWorkout()
+    },
+    getDefaultWorkout() {
+      return {
+        exercises: [this.getDefaultExercise()],
+      }
+    },
+    getDefaultExercise() {
+      return {
+        workout_number: '',
+        title: '',
+        muscle_group_id: null,
+        exercise_type_id: null,
+        sets: [this.getDefaultSet()],
+      }
+    },
+    getDefaultSet() {
+      return {
+        weight: '',
+        repetitions: '',
+        extra: [],
+      }
+    },
+    canAddSet(exercise) {
+      return exercise.sets.length < 6
     },
   },
 }
 </script>
-
-<style scoped>
-/* Стили для формы добавления тренировки */
-</style>
