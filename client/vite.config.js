7364a777bf0149a7bff10e29c1d303cd69f82eb6
Isanin Sergey <sergey.isanin@gmail.com>
2025-02-20 18:06:54 +0300

    build(client): добавлены новые зависимости
    - добавлен eslint-plugin-prettier версии 5.2.1;
    - добавлен eslint-plugin-vue версии 9.30.0;
    - добавлен vite-plugin-vue-devtools версии 7.7.2.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,8 @@
-// Plugins
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
-
-// Utilities
-import { defineConfig } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
+import vueDevTools from 'vite-plugin-vue-devtools'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
@@ -14,15 +13,34 @@ export default defineConfig({
     vuetify({
       autoImport: true,
     }),
+    vueDevTools(), // Добавляем плагин vueDevTools
+    createHtmlPlugin({}), // Добавляем плагин createHtmlPlugin
   ],
-  define: { 'process.env': {} },
+  define: {
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
+    'process.env': {},
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)), // Проверьте этот путь
     },
     extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
   },
   server: {
     port: 3000,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+          return 'app'
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+    outDir: 'dist', // Убедитесь, что этот путь указан правильно
   },
 })
